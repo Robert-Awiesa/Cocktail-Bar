128b51cca41b0231dcf19ef7e404db3ead33ca82
iamrobert <robertawiesa@gmail.com>
2026-03-27 16:59:44 +0000

    Fixed the home page reponsiveness for smaller devices.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import cartIcon from '../assets/cart.avif';
@@ -5,23 +6,43 @@ import { useCart } from '../context/CartContext';
 import './NavBar.css';
 
 function NavBar() {
-const {totalItems} = useCart();
-  return(
-    <header className="header">
-      <img src={logo} alt="Tropical Sips Logo" />
+  const { totalItems } = useCart();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-      <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/menu">Menu</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/cart">
-          <img src={cartIcon} alt="Cart" className='cart-img' />
-          <p className='cart-count'>{totalItems || 0}</p>
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const closeMenu = () => setIsMenuOpen(false);
+
+  return (
+    <header className="header">
+      <div className="header-container">
+        <Link to="/" className="logo-container" onClick={closeMenu}>
+          <img src={logo} alt="Tropical Sips Logo" />
         </Link>
-      </nav>
+
+        {/* Hamburger Icon - Visible only on mobile */}
+        <div className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Navigation Wrapper */}
+        <nav className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+          <div className="nav-links">
+            <Link to="/" onClick={closeMenu}>Home</Link>
+            <Link to="/menu" onClick={closeMenu}>Menu</Link>
+            <Link to="/about" onClick={closeMenu}>About Us</Link>
+            <Link to="/contact" onClick={closeMenu}>Contact</Link>
+          </div>
+
+          <Link to="/cart" className="cart-container" onClick={closeMenu}>
+            <img src={cartIcon} alt="Cart" className='cart-img' />
+            <span className='cart-count'>{totalItems || 0}</span>
+          </Link>
+        </nav>
+      </div>
     </header>
-  )
+  );
 }
 
 export default NavBar;
