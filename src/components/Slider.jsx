6658946c9fbeeeb2274a4c56369dfd4d518a9828
iamrobert <robertawiesa@gmail.com>
2026-04-05 02:34:32 +0000

    corrected slider.cc import

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./slider.css";
+import "./Slider.css";
 import slide1 from "../assets/images/slide-1.jpg";
 import slide2 from "../assets/images/slide-2.jpg";
 import slide3 from "../assets/images/slide-4.jpg";
